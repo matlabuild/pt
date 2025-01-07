552296fe43af25e95c84cca152f2465fde1625fe
Sam <sam@sammatla.com>
2025-01-08 10:31:51 +1300

    move source file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/pt/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: resolve(__dirname, 'src/index.html')
+    }
   }
 }) 
